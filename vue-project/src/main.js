edfcf716d7f6acae672802b77d744d4a84514688
Dennis Eghtedari <year_2000_@hotmail.com>
2023-02-13 16:16:30 +0000

    axiios

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
 
 import './assets/main.css'
 
 const app = createApp(App)
+axios.get('https://swapi.dev/api/').then(resp => {
+
+    console.log(resp.data);
+});
+
 
 app.use(router)
 
