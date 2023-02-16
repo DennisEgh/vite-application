468b8c4f5838d60c01e9aa8bb347f6e48c763366
Dennis Eghtedari <year_2000_@hotmail.com>
2023-02-16 21:02:47 +0000

    added typewriting effect to h1 on homepage

--- vue-project/src/components/LandingPage.vue
+++ vue-project/src/components/LandingPage.vue
@@ -3,33 +3,73 @@ import NavigationBar from "../components/subcomponents/NavigationBar.vue";
 import axios from "axios";
 import { ref } from "vue";
 
-const planetsData = ref(null);
+const weathersData = ref(null);
 
-axios.get("https://swapi.tech/api/planets/").then((resp) => {
-  planetsData.value = resp.data;
-});
+axios
+  .get(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/stockholm?unitGroup=metric&key=BS8GTQN6HJWE3LQ73JQWKDKZX&contentType=json"
+  )
+  .then((resp) => {
+    weathersData.value = resp.data;
+  });
 
-console.log(planetsData);
+console.log(weathersData);
 </script>
 
 <template>
-  <NavigationBar></NavigationBar>
+  <NavigationBar day-greeting=""></NavigationBar>
+
+  <video autoplay muted loop id="myVideo">
+    <source class="video" src="../assets/videoplayback.mp4" type="video/mp4" />
+  </video>
 
   <section id="landing">
-    <video autoplay muted loop id="myVideo">
-      <source
-        class="video"
-        src="../assets/videoplayback.mp4"
-        type="video/mp4"
-      />
-    </video>
+    <header>
+      <div class="header__container">
+        <h1 class="header__greeting">Join The Exploration Today.</h1>
+      </div>
+    </header>
   </section>
 </template>
 
 <style scoped>
+.myVideo,
+video {
+  position: absolute;
+}
 #landing {
   height: 100vh;
   width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+header {
+  height: 200px;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+.header__greeting {
+  color: black;
+  animation: typing 2s steps(22), blink 0.5s step-end infinite alternate;
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: 3px solid;
+  font-family: monospace;
+  font-size: 2em;
+  width: 480px;
+  font-size: 32px;
+  font-weight: bold;
+}
+@keyframes typing {
+  from {
+    width: 0;
+  }
 }
 
+@keyframes blink {
+  50% {
+    border-color: transparent;
+  }
+}
 </style>
--- vue-project/src/components/subcomponents/NavigationBar.vue
+++ vue-project/src/components/subcomponents/NavigationBar.vue
@@ -1,27 +1,32 @@
 <script setup>
 import { RouterLink } from "vue-router";
-</script>
 
+
+defineProps({
+  dayGreeting: {
+    type: String,
+    required: true
+  }
+})
+</script>
 
 <template>
   <div class="navigation">
+    <span> {{ dayGreeting }}</span>
     <nav>
-    
         <div class="nav__logo--container">
             
-            <RouterLink  class="nav__link" to="/Home">
-                <img 
-                class="nav__logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/SpaceX_logo_black.svg/2560px-SpaceX_logo_black.svg.png"
-                alt=""
-                />
-            </RouterLink>
-        </div>
+            <RouterLink class="nav__link" to="/Home">
+          <img
+            class="nav__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/SpaceX_logo_black.svg/2560px-SpaceX_logo_black.svg.png"
+            alt=""
+          />
+        </RouterLink>
+      </div>
     </nav>
-</div>
-<header>
-    <div class="header__container"></div>
-  </header>
+  </div>
+  
 </template>
 
 <style scoped>
@@ -30,7 +35,6 @@ import { RouterLink } from "vue-router";
   z-index: 9999;
   position: absolute;
   width: 100%;
-  height: 80px;
   padding: 20px;
 }
 nav {
@@ -38,7 +42,6 @@ nav {
   justify-content: center;
   height: 100%;
   width: 100%;
-
 }
 .nav__link {
   color: rgb(0, 0, 0);
@@ -48,10 +51,12 @@ nav {
 }
 .nav__logo--container {
   height: 100px;
+  left: 30px;
 }
 .nav__logo {
   height: 50px;
   width: 300px;
+  position: relative;
 }
 
 </style>
